824e1b60f46fd42b8fb4db739343c773c2a6b849
P-Odiix <mahingarodin@gmail.com>
2025-07-14 22:42:54 +0200

    [added the nav links]

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
+import React from "react";
+import Nav from "./Nav";
 import { Button } from "./ui/button";
 
-
 const Header = () => {
-    return <header className="py-8 text-white xl:py-12">
-        <div className="container mx-auto">
-            {/* Logo */}
-            <Link href="/">
-                <h1 className="text-4xl font-semibold">
-                    Rodin<span className="text-accent">.</span>
-                </h1>
-            </Link>
-            {/* desktop nav */}
-        
+  return (
+    <header className="py-8 text-white xl:py-12">
+      <div className="container flex justify-between items-center mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Rodin <span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* desktop nav */}
+        <div className="hidden gap-8 items-center xl:flex">
+          <Nav />
+          <Link href="/contact">
+            <Button variant="default">Hire Me</Button>
+          </Link>
         </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
     </header>
+  );
 };
+
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-	container: {
-		center: true,
-		padding: "15px",
-	},
-	screens: {
-		sm: "640px",
-		md: "768px",
-		lg: "960px",
-		xl: "1200px",
-	},
-	fontFamily: {
-		primary: "var(--font-jetbrainsMono)"
-	},
-  	extend: {
-		colors: {
-			primary: '#1c1c22',
-			accent: {
-				DEFAULT: '#00ff99',
-				hover: '#00e187',
-			}
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "15px",
 		},
-
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)"
+		},
+		extend: {
+			colors: {
+				primary: '#1c1c22',
+				sec: '#222831',
+				accent: {
+					DEFAULT: '#00ff99',
+					hover: '#00e187',
+				}
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+				animation: {
+					"accordion-down": "accordion-down 0.2s ease-out",
+					"accordion-up": "accordion-up 0.2s ease out"
+				}
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 };
