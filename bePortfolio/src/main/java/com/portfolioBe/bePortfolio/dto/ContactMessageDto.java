8f00ee606a9f540ab68d162f4986da030677e0d3
P-Odiix <mahingarodin@gmail.com>
2025-07-15 09:44:14 +0200

    resolve annotation processing issues

--- bePortfolio/src/main/java/com/portfolioBe/bePortfolio/dto/ContactMessageDto.java
+++ bePortfolio/src/main/java/com/portfolioBe/bePortfolio/dto/ContactMessageDto.java
@@ -7,5 +7,37 @@ public class ContactMessageDto {
     private String name;      // Sender's name
     private String email;     // Sender's email
     private String subject;   // Email subject
-    private String message;   // Email content
+    private String message;  // Email content
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
